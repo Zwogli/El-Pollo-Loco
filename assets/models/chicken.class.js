78242dc96ea46e8fa88a5aa3648f54a317a72197
Zwogli <Mathiaskohler@web.de>
2023-06-27 09:04:29 +0200

    animate walking chicken & babyChicken

--- assets/models/chicken.class.js
+++ assets/models/chicken.class.js
@@ -1,4 +1,10 @@
 class Chicken extends MovableObject{
+  IMAGES_WALKING = [
+    './assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+    './assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+    './assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+  ];
+  currentImg = 0;
   x = 200;
   y = 345;
   width = 80;
@@ -7,5 +13,19 @@ class Chicken extends MovableObject{
   constructor(){
     super().loadImage('./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
     this.x = this.x + Math.random() * 500;
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
+
+  animate(){
+    setInterval(() => {
+      let i = this.currentImg % this.IMAGES_WALKING.length; // % = modulo
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imgCache[path];
+      this.currentImg++;
+      if(this.currentImg > 5){
+        this.currentImg = 0;
+      }
+    }, 250);
   }
 }
