84fdd4b5d9b37430ce680bc75c05f06e276ffe5f
Zwogli <Mathiaskohler@web.de>
2023-06-28 08:48:44 +0200

    add moving right & left

--- assets/models/character.class.js
+++ assets/models/character.class.js
@@ -1,36 +1,61 @@
-class Character extends MovableObject{
+class Character extends MovableObject {
   IMAGES_WALKING = [
-      './assets/img/2_character_pepe/2_walk/W-21.png',
-      './assets/img/2_character_pepe/2_walk/W-22.png',
-      './assets/img/2_character_pepe/2_walk/W-23.png',
-      './assets/img/2_character_pepe/2_walk/W-24.png',
-      './assets/img/2_character_pepe/2_walk/W-25.png',
-      './assets/img/2_character_pepe/2_walk/W-26.png',
-    ];
-    currentImg = 0;
+    "./assets/img/2_character_pepe/2_walk/W-21.png",
+    "./assets/img/2_character_pepe/2_walk/W-22.png",
+    "./assets/img/2_character_pepe/2_walk/W-23.png",
+    "./assets/img/2_character_pepe/2_walk/W-24.png",
+    "./assets/img/2_character_pepe/2_walk/W-25.png",
+    "./assets/img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImg = 0;
   x = 100;
   y = 150;
   width = 150;
   height = 280;
   world;
+  speed = 3; //2
 
-  constructor(){
-    super().loadImage('./assets/img/2_character_pepe/2_walk/W-21.png');
+  constructor() {
+    super().loadImage("./assets/img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
     this.animate();
+    let self = this; //! In setIntervall wird this. nicht erkannt !
+    let keyboardIntervall = setInterval(this.keyboardInputs, 1000 / 60, self); // setIntervall( function, time, argument1)
   }
 
-  animate(){
+  animate() {
+    // setInterval(() => {
+    //   if (this.world.keyboard.RIGHT) {
+    //     this.x += this.speed;
+    //   }
+    //   if (this.world.keyboard.LEFT) {
+    //     this.x -= this.speed;
+    //   }
+    // }, 1000 / 60);
+
+    // setInterval(() => {this.keyboardInputs();}, 1000 / 60);
+
     setInterval(() => {
-      let i = this.currentImg % this.IMAGES_WALKING.length; // % = modulo
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imgCache[path];
-      this.currentImg++;
-      if(this.currentImg > 5){
-        this.currentImg = 0;
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        let i = this.currentImg % this.IMAGES_WALKING.length; // % = modulo
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imgCache[path];
+        this.currentImg++;
+        if (this.currentImg > 5) {
+          this.currentImg = 0;
+        }
       }
-    }, 180);
+    }, 100);
   }
 
-  jump(){}
+  keyboardInputs(self){
+    if (self.world.keyboard.RIGHT) {
+      self.x += self.speed;
+    }
+    if (self.world.keyboard.LEFT) {
+      self.x -= self.speed;
+    }
+  }
+
+  jump() {}
 }
