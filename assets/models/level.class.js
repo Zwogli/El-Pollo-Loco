06d7e4513bb285088a300e963f7c885c591831ef
Zwogli <Mathiaskohler@web.de>
2023-07-10 10:41:59 +0200

    refactoring

--- assets/models/level.class.js
+++ assets/models/level.class.js
@@ -8,12 +8,16 @@ class Level{
   backgroundLayers;
 
   constructor(levelArea, enemies, coins, bottles, clouds, backgroundLayers){
-    this.levelArea_start = levelArea[0];
-    this.levelArea_end = levelArea[1];
+    this.levelArea(levelArea);
     this.enemies = enemies;
     this.coins = coins;
     this.bottles = bottles;
     this.clouds = clouds;
     this.backgroundLayers = backgroundLayers;
+  }
+
+  levelArea(levelArea){
+    this.levelArea_start = levelArea[0];
+    this.levelArea_end = levelArea[1];
   }
 }
