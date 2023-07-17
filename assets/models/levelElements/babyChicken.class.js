a664a57252894a9f6ce3f20ccdb05abb3b5972ef
Zwogli <Mathiaskohler@web.de>
2023-07-17 08:04:30 +0200

    refactor

--- assets/models/levelElements/babyChicken.class.js
+++ assets/models/levelElements/babyChicken.class.js
@@ -1,8 +1,8 @@
-class BabyChicken extends MovableObject{
+class BabyChicken extends MovableObject {
   IMAGES_WALKING = [
-    './assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
-    './assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
-    './assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
+    "./assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "./assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "./assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
   ];
   x = 200;
   y = 380;
@@ -12,13 +12,10 @@ class BabyChicken extends MovableObject{
   position_startY = 380;
   world;
 
-  constructor(postion_startX, timeToJump){
-    super().loadImage('./assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
-    this.loadImages(this.IMAGES_WALKING);
-    this.energy = 20;
-    this.x = postion_startX;
-    this.postion_startX = postion_startX;
-    this.speed = .2 + Math.random() * .3;
+  constructor(postion_startX, timeToJump) {
+    super();
+    this.renderImages();
+    this.renderVariables(postion_startX);
     this.applyGravity();
 
     let self = this;
@@ -27,21 +24,37 @@ class BabyChicken extends MovableObject{
     let animationIntervall = setInterval(this.enemieImageAnimation, 100, self);
   }
 
-  enemieJumpAction(self){
- if (!self.isAboveGround()) {
+  /** Load image, images into the imgCache - drawable-objects */
+  renderImages(){
+    this.loadImage("./assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
+    this.loadImages(this.IMAGES_WALKING);
+  }
+
+  /** Set diffrent starting variables
+   * 
+   * @param {number} postion_startX - [levels/level.js] let enemy spawn on this position
+   */
+  renderVariables(postion_startX){
+    this.energy = 20;
+    this.x = postion_startX;
+    this.postion_startX = postion_startX;
+    this.speed = 0.2 + Math.random() * 0.3;
+  }
+
+  /** Intervall method, let object jump - movable-objects*/
+  enemieJumpAction(self) {
+    if (!self.isAboveGround()) {
       self.jump(10);
     }
   }
 
-  /**It´s a setIntervall function for enemie action.
-   * 
-   * @param {object} self - self = this
-   */
-  enemieAction(self){
-      self.movementLoop(self);
+  /**Intervall method, for enemie action */
+  enemieAction(self) {
+    self.movementLoop(self);
   }
 
-  enemieImageAnimation(self){
+  /** Intervall method, image animation */
+  enemieImageAnimation(self) {
     self.playAnimation(self.IMAGES_WALKING);
     self.setFixedPosition();
   }
--- assets/models/levelElements/chicken.class.js
+++ assets/models/levelElements/chicken.class.js
@@ -12,21 +12,37 @@ class Chicken extends MovableObject {
   world;
 
   constructor(postion_startX) {
-    super().loadImage("./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
-    this.energy = 20;
-    this.x = postion_startX;
-    this.postion_startX = postion_startX;
-    this.speed = 0.1 + Math.random() * 0.2;
-    this.loadImages(this.IMAGES_WALKING);
+    super();
+    this.renderImages();
+    this.renderVariables(postion_startX);
     let self = this;
     let actionIntervall = setInterval(this.enemieAction, 1000 / 60, self);
     let animationIntervall = setInterval(this.enemieImageAnimation, 100, self);
   }
 
+  /** Load image, images into the imgCache - drawable-objects */
+  renderImages(){
+    this.loadImage("./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.IMAGES_WALKING);
+  }
+
+  /** Set diffrent starting variables
+   * 
+   * @param {number} postion_startX - [levels/level.js] let enemy spawn on this position
+   */
+  renderVariables(postion_startX){
+    this.energy = 20;
+    this.x = postion_startX;
+    this.postion_startX = postion_startX;
+    this.speed = 0.1 + Math.random() * 0.2;
+  }
+
+  /**Intervall method, for enemie action */
   enemieAction(self) {
     self.movementLoop(self);
   }
 
+  /** Intervall method, image animation */
   enemieImageAnimation(self) {
     self.playAnimation(self.IMAGES_WALKING);
     self.setFixedPosition();
