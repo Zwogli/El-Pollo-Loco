2fc5f7b12e99cefdc003d37e54b19bef5e8018d7
Mathias Kohler <123494012+Zwogli@users.noreply.github.com>
2023-07-22 09:00:12 +0200

    init sound / music

---
+++ assets/js/audio.js
@@ -0,0 +1,21 @@
+// Character sounds
+characterWalkingSound = new Audio('./assets/audio/character/walking.mp3');
+characterJumpSound = new Audio('./assets/audio/character/jump.wav');
+characterHurtSound = new Audio('assets/audio/character/hurt.wav');
+// Endboss sounds
+endbossHurtSound = new Audio('./assets/audio/endboss/endbosshurt.wav');
+endbossDeadSound = new Audio('assets/audio/endboss/endbossdead.wav');
+// Collectable sound
+collectBottleSound = new Audio('./assets/audio/bottle/collectbottle.wav');
+throwBottleSound = new Audio('./assets/audio/bottle/throw.mp3');
+// Background music
+gameMusic = new Audio('./assets/audio/background.mp3');
+
+// ratHurtSound = new Audio('audio/mouse.mp3');
+// bossMusic = new Audio('audio/bossSound.mp3');
+// crackingSound = new Audio('audio/cracking.mp3');
+// winSound = new Audio('audio/winSound.mp3');
+// looseSound = new Audio('audio/looseSound.mp3');
+// menuSound = new Audio('audio/menuSound.mp3');
+
+let muteSoundtoggle = false;
