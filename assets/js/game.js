532ec005f9bd1f4cca663cb9e35e72242f070d73
Mathias Kohler <123494012+Zwogli@users.noreply.github.com>
2023-07-24 22:34:22 +0200

    refactor

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,10 +6,14 @@ let assetsCharacter = new AssetsCharacter();
 let isFullscreen;
 
 function init() {
+  initSettings();
+  canvas = document.getElementById("canvas");
+  canvasWidth = canvas.width; 
+}
+
+function initSettings(){
   checkLandscapeMode();
   fitToScreen();
-  canvas = document.getElementById("canvas");
-  canvasWidth = canvas.width;
   bindBtnsPressEvents();
   volumeControllSound(0.3);
   volumeControllMusic(0.3);
@@ -41,62 +45,11 @@ function setPausableInterval(fn, time) {
   }, 60);
 }
 
-function toggleOptions(){
-  let options = document.getElementById('options-div')
-  options.classList.toggle('d-none');
-  togglePausedInterval(options);
-}
-
-function toggleInstruction(){
-  let instruction = document.getElementById('instruction');
-  instruction.classList.toggle('d-none');
-  togglePausedInterval(instruction);
-}
-
 function togglePausedInterval(id){
   if (id.classList.contains('d-none')) {
     stopInterval = undefined;
   }else{
     stopInterval = true;
-  }
-}
-
-/** Is height > width, use landscape mode */
-function checkLandscapeMode() {
-  if (screen.availHeight > screen.availWidth) {
-    this.document.getElementById("rotateDevice-screen").classList.remove("d-none");
-    this.document.getElementById("screen").classList.add("d-none");
-  } else {
-    this.document.getElementById("rotateDevice-screen").classList.add("d-none");
-    this.document.getElementById("screen").classList.remove("d-none");
-  }
-}
-
-/** If window shrink, add zoom to the screen */
-function fitToScreen(){
-  let bodyWidth = document.getElementById('body').clientWidth;
-  let bodyHeight = document.getElementById('body').clientHeight;
-  if (bodyWidth < 725) {
-      let zoomFactorW = (bodyWidth / 725);
-      document.getElementById('screen').style = `zoom:${zoomFactorW}; ,-moz-transform: scale(${zoomFactorW});`;
-      document.getElementById('controller-div').classList.remove('d-none');
-  } else if (bodyHeight < 485 || isFullscreen) {
-      let zoomFactorH = (bodyHeight / 485);
-      document.getElementById('screen').style = `zoom:${zoomFactorH}; ,-moz-transform: scale(${zoomFactorH});`;
-      document.getElementById('controller-div').classList.remove('d-none');
-  } else {
-      document.getElementById('screen').style = `zoom:1; ,-moz-transform: scale(1);`;
-      document.getElementById('controller-div').classList.add('d-none');
-  }
-}
-
-function toggleFullscreen() {
-  if (isFullscreen) {
-    document.exitFullscreen();
-    isFullscreen = false;
-  } else {
-    document.getElementById("body").requestFullscreen();
-    isFullscreen = true;
   }
 }
 
---
+++ assets/js/screen.js
@@ -0,0 +1,58 @@
+function toggleOptions() {
+  let options = document.getElementById("options-div");
+  options.classList.toggle("d-none");
+  togglePausedInterval(options);
+}
+
+function toggleInstruction() {
+  let instruction = document.getElementById("instruction");
+  instruction.classList.toggle("d-none");
+  togglePausedInterval(instruction);
+}
+
+/** Is height > width, use landscape mode */
+function checkLandscapeMode() {
+  if (screen.availHeight > screen.availWidth) {
+    this.document
+      .getElementById("rotateDevice-screen")
+      .classList.remove("d-none");
+    this.document.getElementById("screen").classList.add("d-none");
+  } else {
+    this.document.getElementById("rotateDevice-screen").classList.add("d-none");
+    this.document.getElementById("screen").classList.remove("d-none");
+  }
+}
+
+/** If window shrink, add zoom to the screen */
+function fitToScreen() {
+  let bodyWidth = document.getElementById("body").clientWidth;
+  let bodyHeight = document.getElementById("body").clientHeight;
+  if (bodyWidth < 725) {
+    let zoomFactorW = bodyWidth / 725;
+    document.getElementById(
+      "screen"
+    ).style = `zoom:${zoomFactorW}; ,-moz-transform: scale(${zoomFactorW});`;
+    document.getElementById("controller-div").classList.remove("d-none");
+  } else if (bodyHeight < 485 || isFullscreen) {
+    let zoomFactorH = bodyHeight / 485;
+    document.getElementById(
+      "screen"
+    ).style = `zoom:${zoomFactorH}; ,-moz-transform: scale(${zoomFactorH});`;
+    document.getElementById("controller-div").classList.remove("d-none");
+  } else {
+    document.getElementById(
+      "screen"
+    ).style = `zoom:1; ,-moz-transform: scale(1);`;
+    document.getElementById("controller-div").classList.add("d-none");
+  }
+}
+
+function toggleFullscreen() {
+  if (isFullscreen) {
+    document.exitFullscreen();
+    isFullscreen = false;
+  } else {
+    document.getElementById("body").requestFullscreen();
+    isFullscreen = true;
+  }
+}
